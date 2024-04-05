96fdb50b213e3a58c4dfe1a972a6d278d8ab5c6c
Drew Payton <drewpayton18@gmail.com>
2024-04-05 16:45:02 -0500

    Adding starter side nav bar with styles and componets files

---
+++ src/componets/Navbar.jsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import '../styles/navbar.css'
+
+
+export default function Navbar() {
+  return (
+  <div className="sidenav">
+    <h3>Drew Payton</h3>
+    <a href="#" className='text-white'>About</a>
+    <a href="#" className='text-white'>Clients</a>
+    <a href="#" className='text-white'>Services</a>
+    <a href="#" className='text-white'>Contact</a>
+  </div>
+  );
+}
