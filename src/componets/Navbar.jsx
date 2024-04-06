a0f49b606a5744113f433e44c832f6c35b4aed08
Drew Payton <drewpayton18@Drews-MacBook-Pro-2.local>
2024-04-05 22:34:37 -0500

    Adding placeholders for socials and added the placeholders for my pages in sidebar.

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -1,15 +1,30 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/navbar.css'
+import userimg from '../assets/userimg.png'
+import githubimg from '../assets/github.png'
+import linkedinIcon from '../assets/linkedin.png'
+import userIcon from '../assets/user.png'
+import letterIcon from '../assets/letter.png'
+import resumeIcon from '../assets/resume.png'
+import programmingIcon from '../assets/web-programming.png'
 
 
 export default function Navbar() {
   return (
-  <div className="sidenav">
-    <h3>Drew Payton</h3>
-    <a href="#" className='text-white'>About</a>
-    <a href="#" className='text-white'>Clients</a>
-    <a href="#" className='text-white'>Services</a>
-    <a href="#" className='text-white'>Contact</a>
-  </div>
+    <div className="sidenav">
+      <h3>Drew Payton</h3>
+      <img id="sidebarPortrait" src={userimg} alt="standard img of default user" />
+      <p>Hello, my name is Drew Payton and I am a software engineer. Welcome to my portfolio</p>
+      <div className='socials'>
+        <a href="https://www.flaticon.com/free-icons/github" title="github icons"><img src={githubimg} alt="Github Image icon" className='invert'/></a>
+        <a href="#" className='text-white'><img src={linkedinIcon} alt="" className='invert'/></a>
+      </div>
+      <div className='sideNavInfo'>
+        <a href="#" className='text-white'><img src={userIcon} alt="" className='invert'/>About Me</a>
+        <a href="#" className='text-white'><img src={programmingIcon} alt="" className='invert'/>Portfolio</a>
+        <a href="#" className='text-white'><img src={resumeIcon} alt="" className='invert'/>Resume</a>
+        <a href="#" className='text-white'><img src={letterIcon} alt="" className='invert'/>Contact</a>
+      </div>
+    </div>
   );
 }
